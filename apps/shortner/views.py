885461a0036466afe005683e225bd47ececee4ee
DavidMartinVergues <dmverges@gmail.com>
2021-09-10 19:02:17 +0200

    first project

---
+++ apps/shortner/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,redirect
+import uuid
+from .models import Url
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_protect 
+import json
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+@csrf_protect
+def create(request):
+    if(request.method=='POST'):
+        post_data = json.loads(request.body.decode("utf-8"))
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=post_data.get('url'), uuid=uid)
+        new_url.save()
+        return JsonResponse({"data":uid})
+
+def go(request,pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
---
+++ static/main.js
@@ -0,0 +1,46 @@
+const shorter_btn = document.getElementById("shorter_btn");
+const url = document.getElementById("url_data");
+const shorter_url = document.getElementById("shorter_url");
+let csrftoken = getCookie("csrftoken");
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+// searching for cookie csrftoken
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+//================================================
+
+shorter_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  postData("/create", { url: url.value }).then((data) => {
+    // JSON data parsed by `response.json()` call
+    shorter_url.innerText = "localhost:8000/" + data.data;
+    shorter_url.href = "http://localhost:8000/" + data.data;
+  });
+});
